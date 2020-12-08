15e71814047f645900f0f689be8487ca151a817c
kushagrakumarovgu <64306534+kushagrakumarovgu@users.noreply.github.com>
2020-12-08 17:57:11 +0100

    since cost is per item. we need to multiply with quantity.

--- invcost.py
+++ invcost.py
@@ -1,4 +1,11 @@
 import pandas as pd
 
 df = pd.read_csv("inventory.csv")
-print("Price: {}".format(sum(df['price'])))
+df_quant = df['quant']
+df_price = df['price']
+cost = 0
+
+for idx in range(len(df_quant)):
+    cost += df_quant[idx] * df_price[idx]
+
+print("Price: {}".format(cost))
